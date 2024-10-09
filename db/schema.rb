f4b987a63ba7070035ada46b0b86926c44ac1430
Martin Berggren <mb.berggren@gmail.com>
2024-10-09 18:49:23 +0200

    Auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_08_201238) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_09_162003) do
   create_table "counters", force: :cascade do |t|
     t.string "title"
     t.string "emoji"
@@ -20,4 +20,23 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_08_201238) do
     t.string "color", default: "#ffffff"
     t.integer "goal", default: 0
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
