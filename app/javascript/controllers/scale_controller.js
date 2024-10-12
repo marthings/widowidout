9d964ba7220ebb313bdf67793d2ed99a05aa2653
Martin Berggren <mb.berggren@gmail.com>
2024-10-12 22:11:18 +0200

    Style fixes

--- app/javascript/controllers/scale_controller.js
+++ app/javascript/controllers/scale_controller.js
@@ -9,20 +9,26 @@ export default class extends Controller {
   }
 
   scaleUp() {
-    this.animateScale(1.2)  // Scale up the amount by 1.2 times
+    this.animateScale(2)  // Scale up the amount by 1.2 times
+    this.amountTarget.style.rotate = "2deg"
+    this.amountTarget.style.translate = "0px -20px"
   }
 
   scaleDown() {
     this.animateScale(0.8)  // Scale down the amount to 80%
+    this.amountTarget.style.rotate = "-2deg"
+    this.amountTarget.style.translate = "0px 20px"
   }
 
   animateScale(scaleValue) {
-    this.amountTarget.style.transition = "transform 0.2s ease-in-out"
+    this.amountTarget.style.transition = "transform 0.3s ease-in-out"
+    
+    
     this.amountTarget.style.transform = `scale(${scaleValue})`
 
     // Reset the scale back to normal after the animation completes
     setTimeout(() => {
       this.amountTarget.style.transform = "scale(1)"
-    }, 200)  // Same duration as transition time
+    }, 300)  // Same duration as transition time
   }
 }
