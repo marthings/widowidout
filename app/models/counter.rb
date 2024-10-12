4650e42490f32fbdf70aa013707c908a10240c0a
Martin Berggren <mb.berggren@gmail.com>
2024-10-12 23:13:05 +0200

    Counters

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -2,6 +2,7 @@ class Counter < ApplicationRecord
   belongs_to :user
 
   validates :amount, numericality: { greater_than_or_equal_to: 0, message: "Can´t go minus, sorry!" }
+  validates :title, presence: true
 
   broadcasts_refreshes
 end
