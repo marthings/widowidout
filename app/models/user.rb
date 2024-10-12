a588bc6a3e0e0f73ec96e34f30d44095e37a9429
Martin Berggren <mb.berggren@gmail.com>
2024-10-12 11:53:41 +0200

    Create an example counter for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :counters, dependent: :destroy
 
   normalizes :email_address, with: -> e { e.strip.downcase }
+
+  after_create :create_example_counter
+
+  private
+
+  def create_example_counter
+    counters.create(title: "Example Counter", amount: 0, goal: 10) # Adjust the attributes as needed
+  end
 end
